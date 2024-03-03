efd31ba9ffd7d4b7db7bb1984597eefd362eb422
vidhu <vidhup430@gmail.com>
2024-03-03 10:31:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./scss/main.scss";
+import "./components/Navbar";
+import "./components/Footer";
+import "./components/Content";
+
+
+
+function App() {
+  return (
+    <>
+       <h1>React</h1>
+    </>
+  )
+}
+
+export default App
